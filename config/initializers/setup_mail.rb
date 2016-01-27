f4da484a2f14a76882767ba76c31175ac2119911
renegomezjr <renegomezjr@gmail.com>
2016-01-27 11:55:40 -0500

    set up mail

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: :plain,
+  user_name: ,
+  password: 
+}
