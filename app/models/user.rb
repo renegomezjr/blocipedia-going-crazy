a6400644c14d0304898d3f55d21ef6c730487da5
renegomezjr <renegomezjr@gmail.com>
2016-02-01 12:49:36 -0500

    Functioning roles and sign up default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :wikis
   after_initialize { self.role ||= :standard }
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
 
 
-  enum[:standard, :premium, :admin]
+  enum role: [:standard, :premium, :admin]
 
 
 end
