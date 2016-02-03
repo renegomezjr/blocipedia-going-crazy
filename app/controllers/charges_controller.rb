0f6ede44bc9ac86a6af03b3a964da513ea31d09a
renegomezjr <renegomezjr@gmail.com>
2016-02-03 14:28:28 -0500

    Customer able to cancel plan

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,34 +3,41 @@ class ChargesController < ApplicationController
 
   def create
 
-    customer = Stripe::Customer.create(
-    email: current_user.email,
-    card: params[:stripeToken],
-    plan: 1
-    )
+    if current_user.customerid.nil?
+      customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken],
+      plan: 1
+      )
+    else
+      customer = Stripe::Customer.retrieve(current_user.customerid)
+      customer.subscriptions.create(:plan => 1)
+    end
 
     flash[:notice] = "Thanks for purchasing a Premium Membership, #{current_user.email}! Enjoy the ability to make private Wikis."
     @user = current_user
     @user.update_attributes(role: 1)
     redirect_to user_path(current_user)
 
-  rescue Stripe::CardError => e
-    flash.now[:alert] = e.message
+  rescue Stripe::CardError
+    flash.now[:alert] = "There was a problem. Please try again."
     redirect_to new_charge_path
   end
 
-  def downgrade
-    customer = Stripe::Customer.retrieve(current_user.email)
+  def destroy
+    Stripe.api_key
+    customer = Stripe::Customer.retrieve(current_user.customerid)
     subscription_id = customer.subscriptions.data.first.id
     customer.subscriptions.retrieve(subscription_id).delete
-    user.update_attributes(role: 0)
+    @user = current_user
+    @user.update_attributes(role: 0)
     redirect_to user_path(current_user)
 
     flash[:notice] = "You successfully downgraded your account."
 
-  rescue Stripe::InvalidRequestError => e
-    flash.now[:alert] = e.message
-    redirect_to edit_user_registration_path(current_user)
+  rescue Stripe::InvalidRequestError
+    flash.now[:alert] = "There was a problem."
+    redirect_to wiki_path
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  delete '/downgrade', to: 'charges#downgrade'
 
   resources :charges
 
