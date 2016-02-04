bf23fa88cf754d66f6a5ee4dd230b6c574934a5e
renegomezjr <renegomezjr@gmail.com>
2016-02-04 18:50:57 -0500

    Wiki accepts and displays markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+      no_intra_emphasis: true,
+      fenced_code_blocks: true,
+      disable_indented_code_blocks: true)
+    return markdown.render(text).html_safe
+  end
 end
